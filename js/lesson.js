967da05b3d7704113a9aaf25292b7e97e6c4b4ef
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-07-11 18:16:05 +0200

    123

---
+++ js/lesson.js
@@ -0,0 +1,24 @@
+// const isAdult = "Чи виповнилося вам 18 років?"
+
+// const result = confirm(isAdult)
+
+// console.log(result);
+
+// const myAnswer = "Введіть ваше ім'я та прізвище";
+
+// const userAnswer = prompt(myAnswer);
+
+// console.log(userAnswer);
+
+// let number = 43;
+// number = number + 10;
+// number += 10;
+// console.log(number);
+
+const myAnswer = "Введіть ваше ім'я";
+
+const userAnswer = prompt(myAnswer);
+
+const answerUppercase = userAnswer.toUpperCase();
+
+console.log(answerUppercase);
