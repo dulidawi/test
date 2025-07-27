68b552c1411c13624f4c2c0ccb67830fce4b95df
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-07-27 12:00:44 +0200

    123

--- js/lesson.js
+++ js/lesson.js
@@ -15,10 +15,50 @@
 // number += 10;
 // console.log(number);
 
-const myAnswer = "Введіть ваше ім'я";
+// const myAnswer = "Введіть ваше ім'я";
 
-const userAnswer = prompt(myAnswer);
+// const userAnswer = prompt(myAnswer);
 
-const answerUppercase = userAnswer.toUpperCase();
+// const answerUppercase = userAnswer.toUpperCase();
 
-console.log(answerUppercase);
+// console.log(answerUppercase);
+
+
+
+// const message = "Hello Wild World!"
+// console.log(message.toUpperCase());
+// console.log(message.toLowerCase());
+// console.log(message.indexOf('Wild'));
+// console.log(message.includes("Hello"));
+// console.log(message.includes("Baba"));
+// console.log(message.startsWith("He"));
+// console.log(message.endsWith("by"));
+// console.log(message.padEnd(56, "Vava"));
+// console.log(message.padStart(34, "Hello"))
+
+const bla = "Sasha"
+const age = 12
+const city = "Warsaw"
+// const text = 'My name is ' + name + ". I'm " + age + " years old." + " I'm from " + city + "."
+const text = `My name is ${bla} , I'm from ${city} , I'm ${age}`
+console.log(text);
+
+
+console.log(`Результат дорівнює ${2 + 2}. `);
+
+console.log(`Десять плюс десять дорівнює ${10 + 10}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
