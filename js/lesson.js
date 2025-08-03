0e8d0d5f31a8e68e2f4d11bbe5bde98fc1d5cd5b
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-08-03 15:09:16 +0200

    123

--- js/lesson.js
+++ js/lesson.js
@@ -1,13 +1,9 @@
 // const isAdult = "Чи виповнилося вам 18 років?"
-
 // const result = confirm(isAdult)
-
 // console.log(result);
 
 // const myAnswer = "Введіть ваше ім'я та прізвище";
-
 // const userAnswer = prompt(myAnswer);
-
 // console.log(userAnswer);
 
 // let number = 43;
@@ -16,14 +12,9 @@
 // console.log(number);
 
 // const myAnswer = "Введіть ваше ім'я";
-
 // const userAnswer = prompt(myAnswer);
-
 // const answerUppercase = userAnswer.toUpperCase();
-
 // console.log(answerUppercase);
-
-
 
 // const message = "Hello Wild World!"
 // console.log(message.toUpperCase());
@@ -36,17 +27,45 @@
 // console.log(message.padEnd(56, "Vava"));
 // console.log(message.padStart(34, "Hello"))
 
-const bla = "Sasha"
-const age = 12
-const city = "Warsaw"
+// const bla = "Sasha"
+// const age = 12
+// const city = "Warsaw"
 // const text = 'My name is ' + name + ". I'm " + age + " years old." + " I'm from " + city + "."
-const text = `My name is ${bla} , I'm from ${city} , I'm ${age}`
-console.log(text);
+// const text = `My name is ${bla} , I'm from ${city} , I'm ${age}`
+// console.log(text);
 
 
-console.log(`Результат дорівнює ${2 + 2}. `);
+// console.log(`Результат дорівнює ${2 + 2}. `);
+// console.log(`Десять плюс десять дорівнює ${10 + 10}`);
 
-console.log(`Десять плюс десять дорівнює ${10 + 10}`);
+
+// Напиши скрипт обробки покупки в магазині.
+
+//   - Баланс користувача зберігається в змінній balance - отримати з інпуту
+//   - Сумма покупки зберігається в змінній payment - отримати з інпуту
+//   - Перед перевіркою вивести повідомлення на екран:
+//   «Загальна вартість замовлення [число] кредитів. Перевіряємо кількість
+//   доступних коштів на рахунку»
+//  - Якщо сума покупки не перевищує баланс:
+//      - Відняти з балансу суму покупки
+//    - Вивести повідомлення «На рахунку залишилося [число] кредитів»
+//  - Якщо сума покупки перевищує баланс:
+//     - Вивести повідомлення «На рахунку недостатньо коштів для проведення операції!»
+//  - В кінці вивести повідомлення «Операція завершена»
+
+// let balance = Number(prompt("Введіть баланс"))
+// let payment = Number(prompt("Введіть суму покупки"))
+// alert(`Загальна вартість замовлення ${payment} кредитів. Перевіряємо кількість доступних коштів на рахунку`)
+ 
+// if (payment < balance) {
+//     balance -= payment
+//     alert(`На рахунку залишилося ${balance} кредитів`);
+// } else {
+//     alert("На рахунку недостатньо коштів для проведення операції!");
+// }
+
+// alert("Операція завершена");
+
 
 
 
---
+++ js/lesson4.js
@@ -0,0 +1,62 @@
+// const num = 3;
+// const result = num > 5 && num < 20;
+// console.log(result);
+
+// const numb = 12
+// const res = numb > 11 || numb < 4
+// console.log(res)
+
+// console.log(!true)
+// console.log(!0);
+
+// let cost = 0
+// const subscription = "pro"
+// if (subscription === "pro") {
+//     cost = 100
+// }
+// console.log(cost);
+
+// let price = 0
+// const subsc = "free"
+// if (subsc === "pro") {
+//     price = 100
+// }
+// console.log(price);
+
+// let price;
+// const subscription = "free"
+// if (subscription === "pro") {
+//     price = 100
+// } else {
+//     price = 0
+// }
+// console.log(price);
+
+let price
+const subscription = "premium";
+if (subscription === "free") {
+    price = 0
+} else if (subscription === "pro") {
+    price = 100;
+} else if (subscription === "premium") {
+    price = 1000
+} else console.log("Error");
+
+console.log(price);
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
