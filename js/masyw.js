3d26a16ef16bcc7a0e25fdcce394a0ca556c7653
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-08-30 11:17:37 +0200

    123

---
+++ js/masyw.js
@@ -0,0 +1,66 @@
+// const numb = [[1, 2, 3], [4, 5, 6], [7, 8, 9],]
+// console.log(numb[1][1]);
+
+
+ // Пошук першого непарного числа
+
+// const numbers = [ 12, 47, 5, 89, 33, 76, 21, 64, 90, 18, 52, 7, 38, 95, 41, 29, 83, 10, 67, 54,];
+
+// for (const q of numbers) {
+//     // console.log(q);
+//     if (q % 2 === 1) {
+//         console.log(q);
+//         break
+//     }
+// }
+
+// Порахувати суму всі додатніх чисел
+
+// const numbers = [
+//     12, -47, 5, 89, 33, -76, 21, 64, -90, 18, 52, 7, -38, 95, 41, 29, 83, 10, -67, 54,];
+// let sum = 0;
+
+// for (const q of numbers) {
+//     if (q > 0) {
+//     sum += q
+//     }
+// }
+
+// console.log(sum);
+
+
+// Знайти мінімальне число
+
+// const numbers = [12, 47, 5, 88, 63, 29, 94, 31, 56, 72, 6, 18, 40, 67, 81, 25, 90, 14, 38, 53,];
+
+// let minimum = numbers[0]
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (minimum > numbers[i]) {
+//         minimum = numbers[i]
+//     }
+// }
+
+// console.log(minimum);
+
+
+// Напиши скрипт який рахує суму елементів двух масивів. Без методу concat()
+
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+
+// let sumArray1 = 0;
+// let sumArray2 = 0;
+
+// for (let i = 0; i < array1.length; i += 1) {
+//     sumArray1 += array1[i]
+// }
+
+// for (let i = 0; i < array2.length; i += 1) {
+//   sumArray += array2[r];
+// }
+
+// let sumArray = 0
+// sumArray = array1 + array2
+
+// console.log(sumArray);
